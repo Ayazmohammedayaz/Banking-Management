d73ba45540c0337f6be57f82d44e29ef1a135697
Mohammed Ayaz <120393995+Ayazmohammedayaz@users.noreply.github.com>
2023-05-10 16:25:38 +0530

    Add files via upload

---
+++ phpfiles/README.md
@@ -0,0 +1,16 @@
+# The-Sparks-Foundation-GRIPAPR-2023
+
+Web Development Internship tasks
+
+Task-1 Basic Banking Website - AT Banks
+
+Languages used-<br>
+1.HTML<br>
+2.CSS<br>
+3.Js<br>
+4.PHP<br>
+5.SQL
+ 
+Software used-<br>
+1.XAMPP<br>
+2.PHPMYADMIN
---
+++ phpfiles/navbar.php
@@ -0,0 +1,10 @@
+<!-- navbar -->
+<nav class="navbar">
+          <h2 class="logo">MOHAMMED Banks</h2>
+          <ul class=items>
+            <li><a href="/phpfiles/index.php">Home</a></li>
+            <li><a href="/phpfiles/users.php">Create User</a></li>
+            <li><a href="/phpfiles/transfermoney.php">Money Transfer</a></li>
+            <li><a href="/phpfiles/transactionhistory.php">Transactions</a></li>
+          </ul>
+        </nav>
